d97d27349247e4079b838dfe337c7881a5e0012e
WINDOWS 10 <WINDOWS 10@AlbertoCR>
2021-04-04 23:27:43 +0200

    04/04

--- Programación/ProgramaciónRelación10/ejercicio1/Equipo.java
+++ Programación/ProgramaciónRelación10/ejercicio1/Equipo.java
@@ -1,5 +1,29 @@
 package ejercicio1;
 
+import java.util.HashSet;
+
 public class Equipo {
 
+	private String nombre;
+	private HashSet<Alumno> jugadores;
+	
+	public Equipo(String nombre) {
+		this.nombre = nombre;
+		jugadores = new HashSet<Alumno>();
+	}
+	
+	public void incorporarAlumno(Alumno alumno) {
+		
+	}
+	
+	public void borrarAlumno(Alumno alumno) {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Equipo [nombre=" + nombre + ", jugadores=" + jugadores + "]";
+	}
+	
+	
 }
